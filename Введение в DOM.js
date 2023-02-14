16dcc89d9f41ea3bf2b3e2cd88ebc1e4f5a10f7c
default <bruh@yandex.ru>
2023-02-14 14:59:10 +0300

    first commit1

--- Введение в DOM.js
+++ Введение в DOM.js
@@ -1,34 +1,112 @@
 // c 239 до 262
-let elem1 = document.querySelectorAll('#elem');
-let input = document.querySelectorAll('#input1');
-let div = document.querySelector('#div');
-let href = document.querySelector('a');
-let img1 = document.querySelector('#img1');
-let img2 = document.querySelector('#img2');
-let button1 = document.querySelector('#button1');
-let button2 = document.querySelector('#button2');
-let ul = document.querySelectorAll('#ul li');
-let arr = [];
-img1.width = 300;
-img2.width = 300;
-button1.value = 1;
-button1.addEventListener('click', func2);
+// let elem1 = document.querySelectorAll('#elem');
+// let input = document.querySelectorAll('#input1');
+// let div = document.querySelector('#div');
+// let href = document.querySelector('a');
+// let img1 = document.querySelector('#img1');
+// let img2 = document.querySelector('#img2');
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let ul = document.querySelectorAll('#ul li');
+// let arr = [];
+// img1.width = 300;
+// img2.width = 300;
+// button1.value = 1;
+// button1.addEventListener('click', func2);
 
-for(let li of ul){
-	li.addEventListener('click', function func(){
-		this.textContent = Number(li.textContent) + 1;
-		if(this.textContent >= 10){
-			this.removeEventListener('click', func);
-		}
-	});
-}
+// for(let li of ul){
+// 	li.addEventListener('click', function func(){
+// 		this.textContent = Number(li.textContent) + 1;
+// 		if(this.textContent >= 10){
+// 			this.removeEventListener('click', func);
+// 		}
+// 	});
+// }
 
-function func2() {
-	this.value = Number(this.value) + 1;
-	if(this.value >= 10){
-		this.removeEventListener('click', func2);
-	}
-}
-console.log(input1.value);
-console.log(input1.getAttribute('value'));
-console.log(href.getAttribute('href'));
+// function func2() {
+// 	this.value = Number(this.value) + 1;
+// 	if(this.value >= 10){
+// 		this.removeEventListener('click', func2);
+// 	}
+// }
+// console.log(input1.value);
+// console.log(input1.getAttribute('value'));
+// console.log(href.getAttribute('href'));
+
+//264
+
+//#1
+
+// let div = document.querySelector('.div');
+
+// let button = document.querySelector('button');
+// button.addEventListener('click', function(){
+// 	div.style.width = '300px';
+// 	div.style.height = '300px';
+// 	div.style.border = '5px solid black';
+// 	div.style.background = 'red';
+// });
+
+//#2
+
+// let div = document.querySelector('.div');
+
+// let button = document.querySelector('button');
+// button.addEventListener('click', function(){
+// 	div.style.fontSize = '20px';
+// 	div.style.borderTop = '5px solid black';
+// 	div.style.background = 'red';
+// });
+
+//#3
+
+// let button = document.querySelector('button');
+// let li = document.querySelectorAll('li')
+
+// button.addEventListener('click', function(){
+// 	li.style.cssFloat = 'left';
+// });
+
+//265
+
+//#2
+
+// let p = document.querySelector('p');
+// let red = document.querySelector('#red');
+// let fat = document.querySelector('#fat');
+// let crossOut = document.querySelector('#crossOut');
+
+// red.addEventListener('click', function(){
+// 	p.classList.add('colorred')
+// });
+// crossOut.addEventListener('click', function(){
+// 	p.classList.add('crossOut')
+// });
+// fat.addEventListener('click', function(){
+// 	p.classList.add('fat')
+// });
+
+//#3
+
+// let p = document.querySelector('p');
+// let red = document.querySelector('#red');
+// let fat = document.querySelector('#fat');
+// let crossOut = document.querySelector('#crossOut');
+
+// red.addEventListener('click', function(){
+// 	p.classList.toggle('colorred')
+// });
+// crossOut.addEventListener('click', function(){
+// 	p.classList.toggle('crossOut')
+// });
+// fat.addEventListener('click', function(){
+// 	p.classList.toggle('fat')
+// });
+
+let button = document.querySelector('#button');
+let elem = document.querySelector('#elem');
+
+button.addEventListener('click', function() {
+	elem.classList.toggle('active');
+});
+
